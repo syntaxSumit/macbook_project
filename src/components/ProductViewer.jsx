0c493a9163ec8e063a35d522c53df7212d9874b7
Sumit <Sumitgiri112341@outlook.com>
2025-10-28 22:34:09 +0530

    models setup

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import useMacbookStore from "../store/store";
+import { clsx } from "clsx";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
+
+const ProductViewer = () => {
+  const { color, scale, setColor, setScale } = useMacbookStore();
+
+  return (
+    <section id="product-viewer">
+      <h2>Take a closer Look.</h2>
+
+      <div className="controls">
+        <p className="info">
+          {" "}
+          MacBookPro {scale} in {color}
+        </p>
+        <div className="flex-center gap-5 mt-5">
+          <div className="color-control">
+            <div
+              onClick={() => setColor("#abd5bd")}
+              className={clsx(
+                "bg-neutral-300",
+                color === "#abd5bd" && "active"
+              )}
+            ></div>
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active"
+              )}
+            ></div>
+          </div>
+          <div className="size-control">
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                scale === 0.06 ? "big-white text-black" : "bg-transparent"
+              )}
+            >
+              <p>14"</p>
+            </div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                scale === 0.08 ? "big-white text-black" : "bg-transparent"
+              )}
+            >
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <ambientLight intensity={0.5} />
+        <MacbookModel14 scale={scale} position={[0,.4,0]} />
+        <OrbitControls  enableZoom={false}  />
+      </Canvas>
+    </section>
+  );
+};
+
+export default ProductViewer;
