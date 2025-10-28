6195a82f2f7aa394dfd140f5b31bf358d0ae8379
Sumit <Sumitgiri112341@outlook.com>
2025-10-28 18:33:46 +0530

    second commit Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from "react";
+
+const Hero = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 1;
+  });
+  return (
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="/title.png" alt=" MacBook Title " />
+      </div>
+
+      <div>
+        <video src="/videos/hero.mp4" autoPlay muted playsInline />
+      <button>buy</button>
+      <p>From $1599 or $1 for life time </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
