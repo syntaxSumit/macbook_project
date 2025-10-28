5abdc3f77c3f9deb8401c245f4ed899cf22b6e65
Sumit <Sumitgiri112341@outlook.com>
2025-10-28 11:17:40 +0530

    first commit Navbar

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iPad" },
+  { label: "iPhone" },
+  { label: "Watch" },
+  { label: "AirPods" },
+  { label: "TV & Home" },
+  { label: "Entertainment" },
+  { label: "Accessories" },
+  { label: "Support" },
+];
